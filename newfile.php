67145a7a230d26b255b2d6ba8bf476aff5be4802
julia <juliazamiths@gmail.com>
2021-09-07 10:22:56 -0300

    primeiro commit

---
+++ newfile.php
@@ -0,0 +1,38 @@
+<?php
+
+require 'vendor/autoload.php';   //precisamos desse autoload.
+require 'buscador.php';
+
+
+use GuzzleHttp\Client;
+use Symfony\Component\DomCrawler\Crawler;
+
+//o Composer tem um arquivo 'autoload.php', que faz o trabalho necessário para definir um autoload de classes de forma que seja possível utilizar
+//as dependências sem incluir seus arquivos separadamente.
+
+//precisamos colocar os requires dados no packagist (de todos os pacotes que usaremos) antes de começar. Eles devem ser feitos no prompt dentro do diretório em que esse arquivo está.
+
+
+
+$client = new Client(['verify' => false,'base_uri' => 'https://www.alura.com.br']);                            //url base. A partir desse site, fazemos uma requisição
+
+$crawler = new Crawler();
+
+
+$buscador = new Buscador($client, $crawler);
+$cursos = $buscador -> buscar('/cursos-online-programacao/php');       
+
+//$cursos vai retornar uma lista com vários elementos da página (1 para cada curso). A partir dessa lista, podemos iterar sobre:
+
+foreach ($cursos as $curso){
+  echo $curso.PHP_EOL;
+
+}
+
+//Passando para o git: Coloca no git ignore o /vendor. Assim, ele não gerenciará essas entradas. 
+/*Aí, dentro do diretório buscador-cursos, damos git init e criamos um commit no bash do git.
+ * aí, criamos a tag de versionamento, tag -a v1.0.0:
+ * O primeiro número é a versão principal, fazendo mudanças API imcompatíveis com as outras versões.
+ * O segundo número são mudanças menores, compatíveis com as outras versões
+ * Correção de bugs: ultimo número.
+ */
